c034517e2bbf1dbd8e7effc4ae7328667d21846a
Carlos Perilla <carlosperillaprogramacion@gmail.com>
2023-06-28 07:49:10 -0300

    Añadiendo 404 a la API

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,11 +1,11 @@
 import { Rooms, Room, RoomData } from "../../types/index.js";
 
 import { getCurrentRooms } from "../../utils/index.js";
-import { Application, Request, Response } from 'express';
+import { Application, Request, Response, NextFunction } from 'express';
 
 export default function api(app: Application): void {
     app.get('/', (req: Request, res: Response) => {
-        res.json({"message": "Bienvenidos personas!"})
+        res.json({"message": "Bienvenidos personas, y animales!"})
     });
     
     app.get('/rooms', async (req: Request, res: Response) => {
@@ -36,4 +36,11 @@ export default function api(app: Application): void {
             res.status(404).json({ error: 'Room not found' });
         }
     });
+
+    app.use((req: Request, res: Response, next: NextFunction) => {
+        res.status(404).json({
+            "error": 404,
+            "message": 'No se pudo encontrar el recurso solicitado.'
+        });
+      });
 }
